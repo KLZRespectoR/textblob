e21c177495bc954fb35232fa76b0815d5cb74d9c
KLZRespectoR <alhmeidi@hotmail.com>
2021-05-28 12:25:26 +0200

    first_full_program

--- main.py
+++ main.py
@@ -1,31 +1,63 @@
-
+print("President’s Name: Abraham Lincoln")
+print("Date of speech: 4 March 1861")
+print("Title of the speech: First Inaugural Address Speech")
+print("Source: https://millercenter.org/the-presidency/presidential-speeches/march-4-1861-first-inaugural-address")
 import nltk as nltk
 text= open('text.txt', encoding='utf-8').read()
-#print(text)
+print("the length of the speech:", len(text))
 import string
 lower_case= text.lower()
 #print(lower_case)
+print("the length of the text on lower case:", len(lower_case))
 
 cleaned_text= lower_case.translate(str.maketrans('','',string.punctuation))
 #print(cleaned_text)
+print("the length of the text without punctuation:", len(cleaned_text))
 from nltk.corpus import wordnet
 from nltk.corpus import stopwords
 sw= stopwords.words('english')
 #print(sw)
 
+from nltk.tokenize import word_tokenize
+tokenized_words= word_tokenize(cleaned_text, "english")
+final_words=[]
+for word in tokenized_words:
+    if word not in sw:
+        final_words.append(word)
+#print(final_words)
+print("the length of the text after tokenization:", len(final_words))
 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+def sentiment_analyse(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    print("NLTK_sentimentintensityanalyzer:", score)
+    neg = score['neg']
+    pos = score['pos']
+    if neg > pos:
+        print("Negative Sentiment")
+    elif pos > neg:
+        print("Positive Sentiment")
+    else:
+        print("Neutral Vibes")
+
+
+
+
+sentiment_analyse(cleaned_text)
 
 from textblob import TextBlob
-#rom vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+#from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 analysis= TextBlob(text)
 #print(dir(analysis))
-
+print(analysis.word_counts['the'])
 #print(analysis.tags)
 noun_phrases= analysis.noun_phrases
-#print(analysis.noun_phrases)
-#print(len(noun_phrases))
+print(analysis.noun_phrases)
+print(len(noun_phrases))
 
-print(analysis.sentiment)
+print("Blobtext_sentimentintensityanalyzer:", analysis.sentiment)
 
 
